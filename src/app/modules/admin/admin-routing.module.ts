ea144b51c5d71f0ad00138338435012e9d3bad4e
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-05-28 13:01:50 +0300

    create user list componsnt

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RoleGuard } from 'src/app/core/guards/role.guard';
+import { ListUserComponent } from './component/users/list/list-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    data: {
+      title: 'Administration',
+    }, 
+    children: [
+      {
+        path: 'import',
+        canActivate: [RoleGuard],
+        component: ListUserComponent,
+        data: {
+          title: 'users',
+          roles: ['ROLE_ADMIN','ROLE_USER'] 
+        },
+      },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { ListUserComponent } from './component/users/list/list-user.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListUserComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule
